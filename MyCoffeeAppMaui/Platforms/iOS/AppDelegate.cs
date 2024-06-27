fd06f6d329725c29e9d374d79aeb7d8d93b02be8
Neha Kumari <46513867+neha993185@users.noreply.github.com>
2024-06-27 23:29:59 +0530

    Xamarin to Maui migration
    Migration is done with the help Upgrade Assistance.
    
    Here Inside XamarinForm folder MyCoffeeApp, MyCoffeeApp.Android, MyCoffeeApp.iOS are xamarin forms projects and MyCoffeeAppCore is maui project created by Upgrade Assistance.
    
    Note: XamarinForm folder is only for reference purpose.
    
    Final MAUI Project is MyCoffeeAppMaui

---
+++ MyCoffeeAppMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,35 @@
+﻿using Foundation;
+using MyCoffeeApp.Helpers;
+using UIKit;
+
+namespace MyCoffeeAppMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
+public class Environment : IEnvironment
+{
+    public void SetStatusBarColor(System.Drawing.Color color, bool darkStatusBarTint)
+    {
+        if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+        {
+            var statusBar = new UIView(UIApplication.SharedApplication.KeyWindow.WindowScene.StatusBarManager.StatusBarFrame);
+            statusBar.BackgroundColor = UIColor.FromRGB(color.R,color.B,color.G);
+            UIApplication.SharedApplication.KeyWindow.AddSubview(statusBar);
+        }
+        else
+        {
+            var statusBar = UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) as UIView;
+            if (statusBar.RespondsToSelector(new ObjCRuntime.Selector("setBackgroundColor:")))
+            {
+                statusBar.BackgroundColor = UIColor.FromRGB(color.R, color.B, color.G);
+            }
+        }
+        var style = darkStatusBarTint ? UIStatusBarStyle.DarkContent : UIStatusBarStyle.LightContent;
+        UIApplication.SharedApplication.SetStatusBarStyle(style, false);
+        Platform.GetCurrentUIViewController()?.SetNeedsStatusBarAppearanceUpdate();
+    }
+}
